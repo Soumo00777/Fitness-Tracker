2559d0ef8832932188ef4060a09e138a1abac6a6
SOUMYAJIT <soumyajitmazumdar5@gmail.com>
2025-06-25 12:31:06 +0530

    Add files via upload

---
+++ client/src/pages/Dashboard.jsx
@@ -0,0 +1,156 @@
+import styled from "styled-components";
+import { counts } from "../utils/data";
+import CountsCard from "../components/cards/CountsCard";
+import WeeklyStatCard from "../components/cards/WeeklyStatCard";
+import CategoryChart from "../components/cards/CategoryChart";
+import { useEffect, useState } from "react";
+import AddWorkout from "../components/AddWorkout";
+import WorkoutCard from "../components/cards/WorkoutCard";
+import { addWorkout, getDashboardDetails, getWorkouts } from "../api";
+
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 22px 0px;
+  overflow-y: scroll;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  max-width: 1400px;
+  display: flex;
+  flex-direction: column;
+  gap: 22px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const Title = styled.h2`
+  margin: 0;
+  padding: 0px 16px;
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+`;
+
+const FlexWrap = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0px 16px;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-bottom: 100px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+function Dashboard() {
+  const [data, setData] = useState();
+  const [buttonLoading, setButtonLoading] = useState(false);
+  const [todaysWorkouts, setTodaysWorkouts] = useState([]);
+  const [workout, setWorkout] = useState(`#Legs
+-Back Squat
+-5 sets
+-15 reps
+-30 kg
+-10 min`);
+
+  const dashboardData = async () => {
+    const token = localStorage.getItem("f-tracker-app-token");
+    await getDashboardDetails(token)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const getTodaysWorkout = async () => {
+    const token = localStorage.getItem("f-tracker-app-token");
+    await getWorkouts(token, "")
+      .then((res) => {
+        setTodaysWorkouts(res?.data?.todaysWorkouts);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const addNewWorkout = async () => {
+    setButtonLoading(true);
+    const token = localStorage.getItem("f-tracker-app-token");
+    await addWorkout(token, { workoutString: workout })
+      .then((res) => {
+        dashboardData();
+        getTodaysWorkout();
+        setButtonLoading(false);
+      })
+      .catch((err) => {
+        alert(err);
+      });
+  };
+
+  useEffect(() => {
+    dashboardData();
+    getTodaysWorkout();
+  }, []);
+
+  return (
+    <Container>
+      <Wrapper>
+        <Title>Dashboard</Title>
+        <FlexWrap>
+          {counts.map((item, ind) => (
+            <CountsCard item={item} key={ind} data={data} />
+          ))}
+        </FlexWrap>
+
+        <FlexWrap>
+          <WeeklyStatCard data={data} />
+          <CategoryChart data={data} />
+          <AddWorkout workout={workout}
+            setWorkout={setWorkout}
+            addNewWorkout={addNewWorkout}
+            buttonLoading={buttonLoading} />
+        </FlexWrap>
+
+        <Section>
+          <Title>Todays Workouts</Title>
+          <CardWrapper>
+          {todaysWorkouts.map((workout, ind) => (
+              <WorkoutCard workout={workout} key={ind} />
+            ))}
+          </CardWrapper>
+        </Section>
+
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Dashboard
---
+++ client/src/pages/Workouts.jsx
@@ -0,0 +1,144 @@
+import styled from "styled-components";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateCalendar } from "@mui/x-date-pickers";
+import WorkoutCard from "../components/cards/WorkoutCard";
+import { useEffect, useState } from "react";
+import { getWorkouts } from "../api";
+import { CircularProgress } from "@mui/material";
+
+const Container = styled.div`
+  flex: 1;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  padding: 22px 0px;
+  overflow-y: scroll;
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  max-width: 1400px;
+  display: flex;
+  gap: 22px;
+  padding: 0px 16px;
+  @media (max-width: 750px) {
+    gap: 12px;
+    flex-direction: column;
+  }
+`;
+
+const Left = styled.div`
+  flex: 0.2 0 75px;
+  height: fit-content;
+  padding: 18px;
+  border: 1px solid ${({ theme }) => theme.text_primary + 20};
+  border-radius: 14px;
+  box-shadow: 1px 6px 20px 0px ${({ theme }) => theme.primary + 15};
+
+  & div {
+    color: ${({ theme }) => theme.text_primary} !important;
+  }
+  & svg {
+    color: ${({ theme }) => theme.text_secondary} !important;
+  }
+  & span {
+    color: ${({ theme }) => theme.primary} !important;
+  }
+  & button {
+    color: ${({ theme }) => theme.text_primary} !important;
+    border-color: ${({ theme }) => theme.text_primary} !important;
+  }
+  & button.Mui-selected {
+    color: ${({ theme }) => theme.white} !important;
+  }
+`;
+
+const Title = styled.h3`
+  font-weight: 600;
+  margin: 0;
+  font-size: 16px;
+  color: ${({ theme }) => theme.primary};
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+`;
+
+const Right = styled.div`
+  flex: 1;
+`;
+
+const CardWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  margin-bottom: 100px;
+  @media (max-width: 600px) {
+    gap: 12px;
+  }
+`;
+
+const Section = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-left: 16px;
+  gap: 22px;
+  @media (max-width: 750px) {
+    gap: 12px;
+    padding: 0px;
+  }
+`;
+
+const SecTitle = styled.h2`
+  margin: 0;
+  font-size: 22px;
+  color: ${({ theme }) => theme.text_primary};
+  font-weight: 500;
+`;
+
+function Workouts() {
+  const [todaysWorkouts, setTodaysWorkouts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [date, setDate] = useState("");
+
+  const getTodaysWorkout = async () => {
+    setLoading(true);
+    const token = localStorage.getItem("f-tracker-app-token");
+    await getWorkouts(token, date ? `?date=${date}` : "").then((res) => {
+      setTodaysWorkouts(res?.data?.todaysWorkouts);
+      setLoading(false);
+    });
+  };
+
+  useEffect(() => {
+    getTodaysWorkout();
+  }, [date]);
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Title>Select Date</Title>
+          <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <DateCalendar onChange={(e) => setDate(`${e.$M + 1}/${e.$D}/${e.$y}`)}/>
+          </LocalizationProvider>
+        </Left>
+        <Right>
+          <Section>
+            <SecTitle>Todays Workout</SecTitle>
+            {loading ? (
+              <CircularProgress />
+            ) : (
+              <CardWrapper>
+                {todaysWorkouts.map((workout, ind) => (
+                  <WorkoutCard workout={workout} key={ind}/>
+                ))}
+              </CardWrapper>
+            )}
+          </Section>
+        </Right>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Workouts
